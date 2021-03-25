b4173fc4728f53845763e1ff70dec9e26c8c05da
Daniel Holguin <39926374+MaxiNite@users.noreply.github.com>
2021-03-24 21:25:55 -0700

    Se modifica clase genero y se agrega jar  de objetosServicios

---
+++ src/objetosNegocio/Genero.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetosNegocio;
+
+/**
+ *
+ * @author Dhtey
+ */
+public class Genero {
+
+    private String genero;
+
+    public Genero() {
+    }
+
+    public Genero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Genero " + "genero= " + genero;
+    }
+}
