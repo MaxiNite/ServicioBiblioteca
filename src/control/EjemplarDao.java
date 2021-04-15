0d54ea6d6da1f866938247a54e3cb73ba28cc2bc
Luis Morquecho <luisiyi1997@gmail.com>
2021-04-14 23:26:18 -0700

    Procedimiento almacenado

--- src/control/EjemplarDao.java
+++ src/control/EjemplarDao.java
@@ -39,40 +39,22 @@ public class EjemplarDao implements IPersistencia<Libro> {
     public void Actualizar(Libro obj) {
          boolean band=false;
         try {
-            CallableStatement cs = cn.prepareCall("{call USP_ACTUALIZAREJEMPLAR(?,?,?,?,?,?,?)}");
-            cs.setInt(1, obj.getCodigo());
+            CallableStatement cs = cn.prepareCall("{call USP_ACTUALIZAREJEMPLAR(?,?,?,?,?,?)}");
+            
             cs.setString(1, obj.getNombre());
-            cs.setString(2, obj.getPaterno());
-            cs.setString(3, obj.getMaterno());
-            cs.setString(4, obj.getDni());
-            cs.setString(5, obj.getGenero());
-            cs.setString(6, obj.getEspecialidad());
+            cs.setString(2, obj.getAutor());
+            cs.setString(3, obj.getDescripccion());
+            cs.setString(4, obj.getGenero());
+            cs.setInt(5, obj.getCantidad());
+            cs.setString(6, obj.toString());
             
             if(cs.executeUpdate()>0)
                 band=true;
             
         } catch (Exception ex) {
-            uti.msj(ex.toString(), 0);
+            ex.getStackTrace();
+            //uti.msj(ex.toString(), 0);
         }
-        
-        return band;
-        /*DELIMITER $$
-CREATE PROCEDURE USP_ACTUALIZARALUMNO
-(
-cod  int,
-nom varchar(25),
-pat varchar(25),
-mat varchar(25),
-dn varchar(8),
-gen varchar(1),
-esp varchar(30)
-)
-BEGIN
-update tb_alumno
-set nombre=nom,paterno=pat,materno=mat,dni=dn,genero=gen,especialidad=esp
-where codigo=cod;
-END$$
-DELIMITER ;*/
         
     }
 
